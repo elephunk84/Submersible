7cdff050792cce5e275ed30468163fdbecf0f3c5
elephunk84 <iaincstott@gmail.com>
2016-08-21 00:15:04 +0100

    update

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo apt-get install python python-dev python-setuptools python-pip pyhton-wheel python-opengl python-opencv python-qt4 python-qt4-gl
+sudo pip install pyforms
+sudo pip install visvis
