db4ec15dfb2e9d2a94f385c467b5b2775e7a8333
elephunk84 <iaincstott@gmail.com>
2016-08-21 17:49:14 +0100

    update

--- sub_control_app.sh
+++ sub_control_app.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
+THISHOST=$(hostname -f)
+if [ $THISHOST = "Iains-Laptop" ]
+then
+    BASEDIR='/home/iainstott/GitRepo/Submersible/'
+if [ $THISHOST = "submersible-pi" ]
+then
+    BASEDIR='/home/pi/Submersible/'
+fi
 pkill xboxdrv >> /dev/null
 sudo pkill xboxdrv >> /dev/null
 git pull >> /dev/null
 sudo rmmod xpad >> /dev/null
-python run.py &
-sudo xboxdrv --config ./resources/xboxdrv.conf
+python ${BASEDIR}/run.py &
+sudo xboxdrv --config ${BASEDIR}/resources/xboxdrv.conf
+fi
