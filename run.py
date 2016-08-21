28fe0f19e1591d7b5423b91b589ff0b42963554b
elephunk84 <iaincstott@gmail.com>
2016-08-21 22:54:02 +0100

    update

--- run.py
+++ run.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python
 ###
 from resources.gui.app import *
+from pythonzenity import Warning
 import resources.XboxController
 import pygame
+import sys
 
 try:
     xboxCont = resources.XboxController.XboxController(controllerCallBack = None, deadzone = 0.1, scale = 1, invertYAxis = False)
+    xboxCont.start()
 except pygame.error:
-    print('error')
+    e = sys.exc_info()[1]
+    Warning(text="PyGame Error: %s" % e)
     
 if __name__ == "__main__":
     gettext.install("app") # replace with the appropriate catalog name
