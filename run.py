3a35601a4933f9ed9d4ea454ed0554329fd85b68
elephunk84 <iaincstott@gmail.com>
2016-08-21 21:50:04 +0100

    update

--- run.py
+++ run.py
@@ -2,13 +2,6 @@
 from resources.gui.app import *
 import resources.XboxController
 
-xboxCont = XboxController.XboxController(
-    controllerCallBack = None,
-    joystickNo = 0,
-    deadzone = 0.1,
-    scale = 1,
-    invertYAxis = False)
-
 if __name__ == "__main__":
     gettext.install("app")
     app = wx.PySimpleApp(0)
@@ -16,4 +9,10 @@ if __name__ == "__main__":
     Sub_Control = MyFrame(None, wx.ID_ANY, "")
     app.SetTopWindow(Sub_Control)
     Sub_Control.Show()
+    xboxCont = XboxController.XboxController(
+        controllerCallBack = None,
+        joystickNo = 0,
+        deadzone = 0.1,
+        scale = 1,
+        invertYAxis = False)
     app.MainLoop()
