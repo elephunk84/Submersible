829fd7d3e2b9356793ac2f4f53b71c9edf04cde2
elephunk84 <iaincstott@gmail.com>
2016-08-21 21:48:09 +0100

    update

--- run.py
+++ run.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python
 from resources.gui.app import *
+import resources.XboxController
+
+xboxCont = XboxController.XboxController(
+    controllerCallBack = None,
+    joystickNo = 0,
+    deadzone = 0.1,
+    scale = 1,
+    invertYAxis = False)
 
 if __name__ == "__main__":
     gettext.install("app")
