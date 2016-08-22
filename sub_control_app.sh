d548160100a2e48b2b833f520b5883e2061a5597
elephunk84 <iaincstott@gmail.com>
2016-08-22 22:16:08 +0100

    update

--- sub_control_app.sh
+++ sub_control_app.sh
@@ -12,6 +12,6 @@ cd ${BASEDIR}
 pkill xboxdrv >> /dev/null
 sudo pkill xboxdrv >> /dev/null
 sudo rmmod xpad >> /dev/null
-sudo xboxdrv --config ./resources/xboxdrv.conf --detach-kernel-driver
+sudo xboxdrv --config ./resources/xboxdrv.conf --detach-kernel-driver >> /dev/null
 python ./run.py 
 # EOF #
