9a086796c1bb518650d1b8125e30cb60ed272f7b
elephunk84 <iaincstott@gmail.com>
2016-08-22 22:22:42 +0100

    update

--- sub_control_app.sh
+++ sub_control_app.sh
@@ -6,12 +6,12 @@ case "${THISHOST}" in
         ;;
     submarine-pi)
         BASEDIR='/home/pi/Submersible/'
+        /usr/bin/xterminal sudo xboxdrv --config ./resources/xboxdrv.conf --detach-kernel-driver 
         ;;
 esac
 cd ${BASEDIR}
 pkill xboxdrv >> /dev/null
 sudo pkill xboxdrv >> /dev/null
 sudo rmmod xpad >> /dev/null
-xterminal -e sudo xboxdrv --config ./resources/xboxdrv.conf --detach-kernel-driver 
 python ./run.py 
 # EOF #
