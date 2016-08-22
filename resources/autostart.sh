9f67904b08b4c0b1cf2ea8058e68de4eb7370c36
elephunk84 <iaincstott@gmail.com>
2016-08-22 23:09:30 +0100

    update

--- resources/autostart.sh
+++ resources/autostart.sh
@@ -2,4 +2,5 @@ pkill xboxdrv
 sudo pkill xboxdrv
 sudo rmmod xpad
 /usr/bin/lxterminal -e 'sudo xboxdrv --config /home/pi/Submersible/resources/xboxdrv.conf'
+/bin/sleep 30
 /usr/bin/lxterminal -e 'sh /home/pi/Submersible/sub_control_app.sh'
