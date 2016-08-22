3ffd7d8fbe29e3115c2d1730faa255f2f73659f3
elephunk84 <iaincstott@gmail.com>
2016-08-22 21:52:27 +0100

    update

--- run.py
+++ run.py
@@ -23,7 +23,7 @@ def setBase():
 def xBoxController():
     if (hostname == 'submarine-pi' ):
         try:
-            subprocess.call('/usr/bin/lxterminal /home/pi/Submersible/resources/XboxController.py')
+            subprocess.call('/usr/bin/lxterminal')
         except subprocess.CalledProcessError as e:
             Error(text=e.output())
     else:
