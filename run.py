6224e3adf2b84140e5c29b4cf9b33316d4504578
elephunk84 <iaincstott@gmail.com>
2016-08-23 21:14:49 +0100

    update

--- run.py
+++ run.py
@@ -27,13 +27,13 @@ def setBase():
     elif ( hostname == 'submarine-pi'):
         basedir=submarinebasedir
         infile_path = "/dev/input/event2"
-        in_file = open(infile_path, "rb")
     os.chdir(basedir)
     sys.path.append(basedir)
             
 if __name__ == "__main__":
     setBase()
     subprocess.Popen('resources/gui/app.py')
+    in_file = open(infile_path, "rb")
     while event:
         (tv_sec, tv_usec, type, code, value) = struct.unpack(FORMAT, event)
     if type != 0 or code != 0 or value != 0:
