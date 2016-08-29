a7365f90945c61543b3ad1a903c3dcc51420b173
elephunk84 <iaincstott@gmail.com>
2016-08-29 18:45:44 +0100

    update

--- run.py
+++ run.py
@@ -32,15 +32,15 @@ def setBase():
 if __name__ == "__main__":
     setBase()
     subprocess.Popen('resources/gui/app.py')
-    infile_path = "/dev/input/event2"
-    in_file = open(infile_path, "rb")
-    while event:
-        (tv_sec, tv_usec, type, code, value) = struct.unpack(FORMAT, event)
-    if type != 0 or code != 0 or value != 0:
-        print("Event type %u, code %u, value %u at %d.%d" % \
-            (type, code, value, tv_sec, tv_usec))
-    else:
-        # Events with code, type and value == 0 are "separator" events
-        print("===========================================")
-        event = in_file.read(EVENT_SIZE)
-    in_file.close()
+    #infile_path = "/dev/input/event2"
+    #in_file = open(infile_path, "rb")
+    #while event:
+        #(tv_sec, tv_usec, type, code, value) = struct.unpack(FORMAT, event)
+    #if type != 0 or code != 0 or value != 0:
+        #print("Event type %u, code %u, value %u at %d.%d" % \
+            #(type, code, value, tv_sec, tv_usec))
+    #else:
+        ## Events with code, type and value == 0 are "separator" events
+        #print("===========================================")
+        #event = in_file.read(EVENT_SIZE)
+    #in_file.close()
