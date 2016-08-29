1795d7c948d5c83cdf16f61e43705c0548a281ca
elephunk84 <iaincstott@gmail.com>
2016-08-29 21:43:09 +0100

    update

--- run.py
+++ run.py
@@ -15,6 +15,12 @@ submarinebasedir='/home/pi/Submersible'
 hostname=(socket.gethostname())
 basedir=''
 
+xboxCont = XboxController.XboxController(
+    controllerCallBack = None,
+    joystickNo = 0,
+    deadzone = 0.1,
+    scale = 1,
+    invertYAxis = False)
 
 def setBase():
     if ( hostname == 'Iains-Laptop'):
@@ -27,18 +33,4 @@ def setBase():
 if __name__ == "__main__":
     setBase()
     subprocess.Popen('resources/gui/app.py')
-    infile_path = "/dev/input/js1"
-    in_file = open(infile_path, "rb")
-    FORMAT = 'llHHI'
-    EVENT_SIZE = struct.calcsize(FORMAT)
-    event = in_file.read(EVENT_SIZE)
-    while event:
-        (tv_sec, tv_usec, type, code, value) = struct.unpack(FORMAT, event)
-    if type != 0 or code != 0 or value != 0:
-        print("Event type %u, code %u, value %u at %d.%d" % \
-            (type, code, value, tv_sec, tv_usec))
-    else:
-        # Events with code, type and value == 0 are "separator" events
-        print("===========================================")
-        event = in_file.read(EVENT_SIZE)
-    in_file.close()
+    
