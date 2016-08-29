bea37b57a5e730a67d68978a91732155221e3b15
elephunk84 <iaincstott@gmail.com>
2016-08-29 21:04:06 +0100

    update

--- run.py
+++ run.py
@@ -10,11 +10,6 @@ from resources.gui.app import *
 import resources.XboxController as XboxController
 from pythonzenity import Warning, Message, Error
 
-#infile_path = ''
-#in_file = ''
-#FORMAT = 'llHHI'
-#EVENT_SIZE = struct.calcsize(FORMAT)
-#event = in_file.read(EVENT_SIZE)
 laptopbasedir='/home/iainstott/GitRepo/Submersible'
 submarinebasedir='/home/pi/Submersible'
 hostname=(socket.gethostname())
@@ -32,15 +27,18 @@ def setBase():
 if __name__ == "__main__":
     setBase()
     subprocess.Popen('resources/gui/app.py')
-    #infile_path = "/dev/input/event2"
-    #in_file = open(infile_path, "rb")
-    #while event:
-        #(tv_sec, tv_usec, type, code, value) = struct.unpack(FORMAT, event)
-    #if type != 0 or code != 0 or value != 0:
-        #print("Event type %u, code %u, value %u at %d.%d" % \
-            #(type, code, value, tv_sec, tv_usec))
-    #else:
-        ## Events with code, type and value == 0 are "separator" events
-        #print("===========================================")
-        #event = in_file.read(EVENT_SIZE)
-    #in_file.close()
+    FORMAT = 'llHHI'
+    EVENT_SIZE = struct.calcsize(FORMAT)
+    event = in_file.read(EVENT_SIZE)
+    infile_path = "/dev/input/event2"
+    in_file = open(infile_path, "rb")
+    while event:
+        (tv_sec, tv_usec, type, code, value) = struct.unpack(FORMAT, event)
+    if type != 0 or code != 0 or value != 0:
+        print("Event type %u, code %u, value %u at %d.%d" % \
+            (type, code, value, tv_sec, tv_usec))
+    else:
+        # Events with code, type and value == 0 are "separator" events
+        print("===========================================")
+        event = in_file.read(EVENT_SIZE)
+    in_file.close()
