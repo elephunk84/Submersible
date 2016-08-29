1f8f9ab71942dbb86d2eb9fcf1a9c6413300e337
elephunk84 <iaincstott@gmail.com>
2016-08-29 19:47:30 +0100

    update

--- resources/autostart.sh
+++ resources/autostart.sh
@@ -1,5 +1,5 @@
 pkill xboxdrv
 sudo pkill xboxdrv
 sudo rmmod xpad
-/usr/local/bin/xboxdrv-daemon.py -v -x /usr/local/bin/xboxdrv --config /home/pi/Submersible/resources/xboxdrv-evdev.conf
+/usr/bin/xboxdrv -D --config /home/pi/Submersible/resources/xboxdrv-evdev.conf --dbus disabled
 /usr/bin/lxterminal -e 'sh /home/pi/Submersible/sub_control_app.sh'
